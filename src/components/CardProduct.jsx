fe11d5d8bcd3a678cd55513f8d2793ae553d9726
Pablo Marino <pmarino2013@gmail.com>
2025-04-03 17:47:06 -0300

    Agrego Link a las tarjetas de productos

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CardProduct = (props) => {
   //   console.log(props.product.image);
 
-  const { image, title } = props.product;
+  const { image, title, id } = props.product;
   return (
     <div className="col">
-      <div className="card h-100">
-        <img src={image} className="card-img-top card-image" alt={title} />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+      <Link to={`/product/${id}`}>
+        <div className="card h-100">
+          <img src={image} className="card-img-top card-image" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
